bdcbd8b2f5103093953cfac1bd14f515c4de4b68
Wyller21 <92105095+Wyller21@users.noreply.github.com>
2021-10-13 20:34:52 -0300

    Update calculadora_idade_1.0.py

--- calculadora_idade_1.0.py
+++ calculadora_idade_1.0.py
@@ -1,32 +1,10 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[2]:
-
-
-print ("Hello, Python!")
-
-
-# In[ ]:
-
 
 print ("Olá, Vamos Calcular sua idade!")
 anoatual = int(input("Em qual ano estamos? "))
 anonascimento = int(input("Em qual ano você nasceu? "))
 idadeatual = (anoatual - anonascimento)
 print ("Sua Idade é:", idadeatual, "anos")
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
 
